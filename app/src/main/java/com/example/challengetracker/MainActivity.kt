7f8d828d842ff38418472b9a5df0a4773cee74c6
Ulrike <uengeln@gmail.com>
2020-11-20 11:55:23 +0200

    maps fragment added

--- app/src/main/java/com/example/challengetracker/MainActivity.kt
+++ app/src/main/java/com/example/challengetracker/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.challengetracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var mMap: GoogleMap
+    private val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    private val REQUEST_LOCATION = 7
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
