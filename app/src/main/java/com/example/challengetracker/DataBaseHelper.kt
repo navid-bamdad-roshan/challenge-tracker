8974294527dcbfdf02c39b23d4f74837ea4f16a7
navid-bamdad-roshan <navid.bamdadroshan@gmail.com>
2020-11-23 00:31:08 +0200

    develop DataBaseHelper

---
+++ app/src/main/java/com/example/challengetracker/DataBaseHelper.kt
@@ -0,0 +1,251 @@
+package com.example.challengetracker
+
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+object DataBaseHelper{
+
+    init {
+    }
+
+
+
+    private lateinit var applicationContext: Context
+
+    private lateinit var db: FirebaseFirestore
+
+    private val sharedPreferencesFileKey = "challenge_tracker"
+    private val curChallengeNameKey = "current_challenge_name"
+    private val curActivityNameKey = "current_activity_name"
+    private val curChallengeIdKey = "current_challenge_id"
+    private val curActivityIdKey = "current_activity_id"
+    private val nicknameKey = "nickname"
+
+
+
+    private val challengesCollectionName = "challenges"
+    private val challengeActivitiesCollectionName = "challengeActivities"
+    private val userActivitiesCollectionName = "userActivities"
+
+
+    // To set the application context
+    fun setAppContext(context: Context){
+        applicationContext = context
+        db = Firebase.firestore
+    }
+
+    // Set currently selected challenge name and id in shared preferences
+    fun setCurrentChallenge(currentChallengeName:String, currentChallengeId: String){
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(curChallengeIdKey, currentChallengeId)
+        editor.putString(curActivityNameKey, currentChallengeName)
+        editor.commit()
+    }
+
+    // Get currently selected challenge name from shared preferences
+    fun getCurrentChallengeName(): String{
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val currentChallenge = sharedPref.getString(curChallengeNameKey,"").toString()
+        return(currentChallenge)
+    }
+
+    // Get currently selected challenge id from shared preferences
+    fun getCurrentChallengeId(): String{
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val currentChallenge = sharedPref.getString(curChallengeIdKey,"").toString()
+        return(currentChallenge)
+    }
+
+
+
+
+
+    // Set currently selected activity name and id in shared preferences
+    fun setCurrentActivityType(currentActivityName:String, currentActivityId: String){
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(curActivityIdKey, currentActivityId)
+        editor.putString(curActivityNameKey, currentActivityName)
+        editor.commit()
+    }
+
+    // Get currently selected activity name from shared preferences
+    fun getCurrentActivityName(): String{
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val currentActivity = sharedPref.getString(curActivityNameKey,"").toString()
+        return(currentActivity)
+    }
+
+    // Get currently selected activity id from shared preferences
+    fun getCurrentActivityId(): String{
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val currentActivity = sharedPref.getString(curActivityIdKey,"").toString()
+        return(currentActivity)
+    }
+
+
+
+    // Set current nickname in shared preferences
+    fun setNickname(nickname:String){
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(nicknameKey, nickname)
+        editor.commit()
+    }
+
+    // Get current nickname from shared preferences
+    fun getNickname():String{
+        val sharedPref = applicationContext.getSharedPreferences(sharedPreferencesFileKey, Context.MODE_PRIVATE)
+        val nickname = sharedPref.getString(nicknameKey,"").toString()
+        return(nickname)
+    }
+
+
+
+
+
+    fun addNewChallenge(challenge: Challenge, insertionFinished: (challengeId: String)->Unit){
+        val challengesCollection = db.collection(challengesCollectionName)
+        val tempChallenge = hashMapOf(
+            "name" to challenge.name,
+            "deadline" to challenge.deadline.toString()
+        )
+        val adding = challengesCollection.add(tempChallenge).addOnSuccessListener { it ->
+            var tempChallengeId = it.id
+            val challengeActivityCollection = db.collection(challengeActivitiesCollectionName)
+            challenge.activities.map {
+                val tempChallengeActivity = hashMapOf(
+                        "name" to it.name,
+                        "challengeId" to tempChallengeId,
+                        "pointPerKm" to it.pointPerKm
+                )
+                challengeActivityCollection.add(tempChallengeActivity)
+            }
+            insertionFinished(tempChallengeId)
+        }
+    }
+
+
+
+
+    fun addNewUserActivity(userNickname: String, points:Float, challengeId:String, challengeActivityId:String, insertionFinished: (userActivityId: String)->Unit){
+        val userActivityCollection = db.collection(userActivitiesCollectionName)
+        val tempUserActivity = hashMapOf(
+                "username" to userNickname,
+                "points" to points,
+                "challengeId" to challengeId,
+                "challengeActivityId" to challengeActivityId
+        )
+        userActivityCollection.add(tempUserActivity).addOnSuccessListener {
+            insertionFinished(it.id)
+        }
+    }
+
+
+    fun getUserActivitiesByUsername(){
+        TODO()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getChallengeById(challengeId: String, getResult: (challenge: Challenge)->Unit){
+        val challengesCollection = db.collection(challengesCollectionName)
+        challengesCollection.document(challengeId).get().addOnSuccessListener { it ->
+            val date = LocalDate.parse(it.data?.get("deadline").toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            var tempChallenge = Challenge(name = it.data?.get("name").toString(), deadline = date, id= it.id)
+
+            // getting challenge activities
+            val challengeActivityCollection = db.collection(challengeActivitiesCollectionName)
+            challengeActivityCollection.whereEqualTo("challengeId", tempChallenge.id).get().addOnSuccessListener { activitiesResult->
+                activitiesResult.documents.map {activity->
+                    val tempName = activity.data?.get("name").toString()
+                    val tempPointPerKm = activity.data?.get("pointPerKm").toString().toFloat()
+                    val tempId = activity.id
+                    tempChallenge.activities.add(ChallengeActivity(name = tempName, pointPerKm = tempPointPerKm, id = tempId))
+                }
+                getResult(tempChallenge)
+            }
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getAllChallenges(getResult: (ArrayList<Challenge>) -> Unit){
+        val challengesCollection = db.collection(challengesCollectionName)
+        challengesCollection.get().addOnSuccessListener { allChallenges ->
+            val challengesArray = arrayListOf<Challenge>()
+            allChallenges.documents.mapIndexed { challengeIndex, challengeInstance ->
+                val date = LocalDate.parse(
+                    challengeInstance.data?.get("deadline").toString(),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                )
+                var tempChallenge = Challenge(
+                    name = challengeInstance.data?.get("name").toString(),
+                    deadline = date
+                )
+                tempChallenge.id = challengeInstance.id
+                challengesArray.add(tempChallenge)
+            }
+
+
+            // getting challenge activities
+            val challengeActivityCollection = db.collection(challengeActivitiesCollectionName)
+            challengeActivityCollection.get().addOnSuccessListener {challengeActivitiesResult ->
+
+                val tempChallengeActivityArray = arrayListOf<ChallengeActivity>()
+
+                challengeActivitiesResult.documents.map {
+                    val tempName = it.data?.get("name").toString()
+                    val tempPointPerKm = it.data?.get("pointPerKm").toString().toFloat()
+                    val tempId = it.id
+                    tempChallengeActivityArray.add(ChallengeActivity(
+                        name = tempName,
+                        pointPerKm = tempPointPerKm,
+                        id = tempId
+                    ))
+                }
+
+                challengesArray.mapIndexed { challengeIndex, challengeInstance ->
+                    tempChallengeActivityArray.filter { it -> it.id == challengeInstance.id }.map {
+                        challengesArray[challengeIndex].activities.add(it)
+                    }
+
+                }
+
+                getResult(challengesArray)
+            }
+        }
+    }
+
+
+    fun getAllActivitiesByNickname(){
+        TODO()
+    }
+
+
+    fun getLeadingPointByChallengeName(){
+        TODO()
+    }
+
+}
+
+data class ChallengeActivity(var name: String, var pointPerKm: Float, var id:String = ""){
+
+}
+
+data class Challenge(var name: String, var deadline: LocalDate, var id: String = ""){
+    var activities = arrayListOf<ChallengeActivity>()
+}
+
+data class UserActivity(var username:String, var points:Float, var id:String = ""){
+
+}
+
--- app/src/main/java/com/example/challengetracker/MainActivity.kt
+++ app/src/main/java/com/example/challengetracker/MainActivity.kt
@@ -1,17 +1,149 @@
 package com.example.challengetracker
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DataBaseHelper.setAppContext(this.applicationContext)
+
         btn_start_activity.setOnClickListener {
             intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
+
+
+
+//        val date = LocalDate.parse("2021-12-03", DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+//
+//        //Log.i("logg", date.toString())
+//
+//        var challenge = Challenge("test challenge 2", date)
+//        challenge.activities.add(ChallengeActivity("challenge activity 1", 2F))
+//        challenge.activities.add(ChallengeActivity("challenge activity 2", 3F))
+//
+//
+
+
+
+//        // Insert new challenge
+//        val scope = CoroutineScope(Dispatchers.Default)
+//        scope.launch {
+//            DataBaseHelper.addNewChallenge(challenge, ::onInsertFinished)
+//        }
+
+
+
+//        Insert new user activity
+//        val scope = CoroutineScope(Dispatchers.Default)
+//        scope.launch {
+//            DataBaseHelper.addNewUserActivity("tempUser", 10F, "0yQPsdExBSAcdC3nCQUe", "TH9T3N5LwL7OUJU5Hvjy", ::onInsertFinished)
+//        }
+
+
+//        // Get getChallengeById
+//        val scope = CoroutineScope(Dispatchers.Default)
+//        scope.launch {
+//            DataBaseHelper.getChallengeById("eCYl9TuShYqjQEfvfLiR", ::onResult)
+//        }
+
+        //RlIWGWIdBSD6mrxGra4w
+
+
+//        // Get all Challenges
+//        val scope = CoroutineScope(Dispatchers.Default)
+//        scope.launch {
+//            DataBaseHelper.getAllChallenges {challenges ->
+//                challenges.map {challenge ->
+//                    Log.i("logg", "challenge id: "+challenge.id)
+//                    Log.i("logg", "challenge name: "+challenge.name)
+//                    Log.i("logg", "challenge num activities: "+ challenge.activities.size.toString())
+//                    challenge.activities.map {
+//                        Log.i("logg", "challenge activity name: "+it.name)
+//                        Log.i("logg", "challenge activity point per km: "+it.pointPerKm)
+//
+//
+//                    }
+//                }
+//            }
+//        }
+
+
+//        DataBaseHelper.getAllChallenges {challenges ->
+//            challenges.map {challenge ->
+//                Log.i("logg", "challenge id: "+challenge.id)
+//                Log.i("logg", "challenge name: "+challenge.name)
+//                Log.i("logg", "challenge num activities: "+ challenge.activities.size.toString())
+//                challenge.activities.map {
+//                    Log.i("logg", "challenge activity name: "+it.name)
+//                    Log.i("logg", "challenge activity point per km: "+it.pointPerKm)
+//
+//
+//                }
+//            }
+//        }
+
+
+
+
+        //DataBaseHelper.setCurrentActivityType("activity test 2")
+        //DataBaseHelper.setCurrentChallenge("challenge test")
+
+        //val cc = DataBaseHelper.getCurrentChallenge()
+        //val ca = DataBaseHelper.getCurrentActivityType()
+
+
+        //Log.i("logg", "current challenge is:"+cc)
+        //Log.i("logg", "current activity is:"+ca)
     }
+
+
+//    fun onInsertFinished(id:String){
+//        Log.i("logg", id.toString())
+//    }
+//
+//
+//    private fun onResult(challenge: Challenge){
+//
+//        Log.i("logg", "challenge id: "+challenge.id)
+//        Log.i("logg", "challenge name: "+challenge.name)
+//        Log.i("logg", "challenge num activities: "+ challenge.activities.size.toString())
+//        challenge.activities.map {
+//            Log.i("logg", "challenge activity name: "+it.name)
+//            Log.i("logg", "challenge activity point per km: "+it.pointPerKm)
+//
+//
+//        }
+
+
+//    private fun onResult(challenges: ArrayList<Challenge>){
+//
+//        challenges.map {challenge ->
+//        Log.i("logg", "challenge id: "+challenge.id)
+//        Log.i("logg", "challenge name: "+challenge.name)
+//        Log.i("logg", "challenge num activities: "+ challenge.activities.size.toString())
+//        challenge.activities.map {
+//            Log.i("logg", "challenge activity name: "+it.name)
+//            Log.i("logg", "challenge activity point per km: "+it.pointPerKm)
+//
+//
+//            }
+//        }
+//
+//    }
 }
