3b370a7631e62b779896c51beac9484b9692858a
Ulrike <uengeln@gmail.com>
2020-11-28 15:42:36 +0200

    challenge activity fixed

--- app/src/main/java/com/example/challengetracker/NewChallengeActivity.kt
+++ app/src/main/java/com/example/challengetracker/NewChallengeActivity.kt
@@ -1,14 +1,11 @@
 package com.example.challengetracker
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class NewChallengeActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_new_challenge)
-        //setContentView(R.layout.activity_maps)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_challenge2)
     }
 }
