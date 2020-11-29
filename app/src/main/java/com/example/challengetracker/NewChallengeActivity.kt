d3d5024b9747cb6ce50df7f8feda8d63d8234a91
K-WS <karl-walter@hotmail.com>
2020-11-29 15:11:00 +0200

    Add Challenge View

--- app/src/main/java/com/example/challengetracker/NewChallengeActivity.kt
+++ app/src/main/java/com/example/challengetracker/NewChallengeActivity.kt
@@ -2,10 +2,21 @@ package com.example.challengetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_challenge.*
 
 class NewChallengeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_challenge)
+
+
+        button_CreateChallenge.setOnClickListener {
+            val chal = Challenge(et_ChallengeName.text.toString(),
+                                et_Date.text.toString(),
+                                et_GoalPoints.text.toString().toFloat(),
+                                "testerID")
+            DataBaseHelper.addNewChallenge(chal) {}
+            finish()
+        }
     }
 }
