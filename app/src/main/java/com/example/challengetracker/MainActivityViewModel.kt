a90dff270075d67a65c75bc97ff6cae998172fa0
Ulrike <uengeln@gmail.com>
2020-12-10 10:44:03 +0200

    produced error before

--- app/src/main/java/com/example/challengetracker/MainActivityViewModel.kt
+++ app/src/main/java/com/example/challengetracker/MainActivityViewModel.kt
@@ -99,16 +99,6 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
 
     var userPoints = 0F
     var leadingPoint = 0F
-<<<<<<< HEAD
-=======
-    var totalPoints = 0F
-
-
-
-
-
-
->>>>>>> e843410a06550897440df4039403638196bcdd61
 }
 
 
